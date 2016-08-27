bdc74f3588074a5dc32bf2b0a7d4e1c0f1ee5740
Kevin McNamara <kdmcnamara@wisc.edu>
2016-08-27 00:22:32 -0500

    10

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 var uri = 'mongodb://heroku_21kcp525:aefi8j2lk71bmvv5j8t7bfbqt1@ds017636.mlab.com:17636/heroku_21kcp525';
 
-mongoose.connect(process.env.MONGOLAB_YELLOW_URI || 'mongodb://localhost/big-dill-mean', function(err) {
+mongoose.connect('mongodb://heroku_qmbrfmmn:qabvskpp2nkn4u92jgpdiv6t29@ds017736.mlab.com:17736/heroku_qmbrfmmn' || 'mongodb://localhost/big-dill-mean', function(err) {
 	if(err) {
 		console.log('Failed Connecting to Mongodb');
 	} else {
