2658bf02e2256c221d9929c8fca08763905c1dcb
Kevin McNamara <kdmcnamara@wisc.edu>
2016-08-26 23:36:21 -0500

    Configuring again for deployment

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 var uri = 'mongodb://heroku_21kcp525:aefi8j2lk71bmvv5j8t7bfbqt1@ds017636.mlab.com:17636/heroku_21kcp525';
 
-mongoose.connect(process.env.uri || 'mongodb://localhost/big-dill-mean', function(err) {
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/big-dill-mean', function(err) {
 	if(err) {
 		console.log('Failed Connecting to Mongodb');
 	} else {
