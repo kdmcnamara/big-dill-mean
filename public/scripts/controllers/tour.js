d1da540d0342d8eea24e3ce7535b47362c05905f
Kevin McNamara <kdmcnamara@wisc.edu>
2018-11-02 18:02:02 -0500

    tour dates

--- public/scripts/controllers/tour.js
+++ public/scripts/controllers/tour.js
@@ -24,44 +24,8 @@ angular.module('bigDillApp')
 
     $scope.shows = [
         {
-            name: "Harmony Bar and Grille - Madison, WI",
-            date: "July 21, 2018"
-        },
-        {
-            name: "High Noon Saloon - Madison, WI",
-            date: "August 17, 2018"
-        },
-        {
-            name: "Linneman's Riverwest Inn - Milwaukee, WI",
-            date: "August 18, 2018"
-        },
-        {
-            name: "Gabe's - Iowa City, IA",
-            date: "August 19, 2018"
-        },
-        {
-            name: "Sancho's - Denver, CO",
-            date: "August 20, 2018"
-        },
-        {
-            name: "Be On Key - Denver, CO",
-            date: "August 22, 2018"
-        },
-        {
-            name: "Californos - Kansas City, KS",
-            date: "August 23, 2018"
-        },
-        {
-            name: "Melody Inn - Indianapolis, IN",
-            date: "August 29, 2018"
-        },
-        {
-            name: "Elbo Room - Chicago, IL",
-            date: "August 30, 2018"
-        },
-        {
-            name: "Ruby Lounge - Madison, WI",
-            date: "August 31, 2018"
+            name: "Hippie Hideout - Madison, WI",
+            date: "November 12, 2018"
         }
     ];
 
